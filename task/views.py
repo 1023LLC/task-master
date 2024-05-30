3448c6cad0b691f3b89a2fe4086a0e22014fffce
julius <nyerereoffice@gmail.com>
2024-05-30 09:13:47 +0300

    Render

--- task/views.py
+++ task/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+from .models import Task
+
 def index(request):
-    return render(request, 'task/index.html')
+    tasks = Task.objects.filter(is_done=False)
+    return render(request, 'task/index.html', {'tasks': tasks})
